8407b121eeb77b1c28cca26de1dce8901a5fd5c2
Greg Rearden <gregoryrearden@gmail.com>
2025-09-07 15:10:41 -0400

    build: tinker with env more (#6)

--- api/src/sequelize.ts
+++ api/src/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL || '', {
+const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION || '', {
   // TODO add custom logging
   logging: false,
 });
