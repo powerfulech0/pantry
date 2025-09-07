7419d6c00e7f0a05d302d59bd482fd9661da51ed
powerfulech0 <gregoryrearden@gmail.com>
2025-09-07 13:03:18 -0400

    build: move env vars to own file and ignore in git

--- api/src/sequelize.ts
+++ api/src/sequelize.ts
@@ -1,6 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+const user = process.env.POSTGRES_USER || 'user';
+const password = process.env.POSTGRES_PASSWORD || 'password';
+const db = process.env.POSTGRES_DB || 'pantry';
+const host = process.env.POSTGRES_HOST || 'localhost';
+const port = process.env.POSTGRES_PORT || '5432';
+
+const POSTGRES_CONNECTION_STRING = `postgres://${user}:${password}@${host}:${port}/${db}`;
+
+const sequelize = new Sequelize(POSTGRES_CONNECTION_STRING, {
   // TODO add custom logging
   logging: false,
 });
